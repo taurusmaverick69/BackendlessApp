051740ed551031d87fa1e5ee70470587e0a1b28d
Vladislav <taurusmaverick69@gmail.com>
2017-03-21 23:31:05 +0200

    clean code

--- src/main/java/com/maverick/Runner.java
+++ src/main/java/com/maverick/Runner.java
@@ -5,14 +5,13 @@ import com.maverick.ui.frame.LoginFrame;
 import de.javasoft.plaf.synthetica.SyntheticaPlainLookAndFeel;
 
 import javax.swing.*;
-import java.io.IOException;
 import java.text.ParseException;
 
 import static com.maverick.utils.BackendlessUtils.*;
 
 public class Runner {
 
-    public static void main(String[] args) throws ParseException, UnsupportedLookAndFeelException, IOException {
+    public static void main(String[] args) throws ParseException, UnsupportedLookAndFeelException {
 
         Backendless.initApp(APP_ID, SECRET_KEY, VERSION);
         UIManager.setLookAndFeel(new SyntheticaPlainLookAndFeel());
